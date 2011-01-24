0768332ef8fbf7048b6336581970fdb4d95c32df
Nathaniel R Reindl <nrr@corvidae.org>
2011-01-24 15:57:39 +0000

    Add a script to help with moving in.

---
+++ move-in.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# Download the bash environment from fossil.moyi.us and install them to $HOME.
+#
+# Usage: curl -LsSf http://fossil.moyi.us/nrr-dotfiles-bash-environment/doc/trunk/move-in.sh | sh -
+#
+
+TD=`mktemp -d XXXXXXX`
+REPO_NAME="nrr-dotfiles-bash-environment"
+
+maybe_create_ssh_directory() {
+	if [ ! -d $HOME/.ssh ]
+	then
+		mkdir $HOME/.ssh
+		chmod u=rwx,og-rwx $HOME/.ssh
+	fi
+}
+
+pull_files_from_fossil() {
+	# This relies on TD above.
+
+	repo_host="fossil.moyi.us"
+	repo_name=${REPO_NAME}
+	repo_path="/zip/${repo_name}-tip.zip?uuid=tip"
+	url="http://${repo_host}/${repo_name}/zip/${repo_name}-tip.zip?uuid=tip"
+
+	#curl -LsSf http://fossil.moyi.us/nrr-dotfiles-bash-environment/zip/nrr-dotfiles-bash-environment-tip.zip?uuid=tip > /tmp/nrr-dotfiles-bash-environment-tip.zip
+	curl -LsSf $url > $TD/${repo_name}-tip.zip
+
+	pushd $TD
+	unzip nrr-dotfiles-bash-environment-tip.zip
+	popd
+}
+
+install_into_HOME() {
+	# This relies on TD above.
+
+	pushd $TD/$REPO_NAME-tip/src
+	for f in dot.*
+	do
+		echo ${f/dot./.}
+	done
+	popd
+}
+
+pull_files_from_fossil
+install_into_HOME
