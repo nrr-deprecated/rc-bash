68ca6526d0724d80ef660e2e9459d18ea229bf74
Nathaniel R Reindl <nrr@corvidae.org>
2011-01-24 18:51:31 +0000

    Add in the perlbrew environment settings.

---
+++ src/dot.bashrc.d/perlbrew.sh
@@ -0,0 +1,11 @@
+# $HOME/.bashrc.d/perlbrew.sh
+# Install the perlbrew environment.
+
+# env PERLBREW_ROOT=$HOME/.perlbrew perlbrew init
+
+[[ -s "$HOME/.perlbrew/etc/bashrc" ]] && source "$HOME/.perlbrew/etc/bashrc"
+
+# Local Variables:
+# mode:shell-script
+# End:
+
