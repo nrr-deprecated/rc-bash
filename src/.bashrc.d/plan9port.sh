7444a22002548b4407f562bde293d48f7bddd59f
Nathaniel R Reindl <nrr@corvidae.org>
2011-10-19 13:57:57 +0000

    Add support for Russ Cox's Plan 9 from User Space.

---
+++ src/.bashrc.d/plan9port.sh
@@ -0,0 +1,9 @@
+# $HOME/.bashrc.d/plan9port.sh
+# Support for Plan 9 from User Space
+
+PLAN9=$HOME/opt/plan9port/current export PLAN9
+PATH=$PATH:$PLAN9/bin export PATH
+
+# Local Variables:
+# mode:shell-script
+# End:
