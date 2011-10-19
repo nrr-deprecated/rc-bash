fbefbe3eaa92e699dcd4bd0571579c62e2f59d48
Nathaniel R Reindl <nrr@corvidae.org>
2011-10-19 13:22:50 +0000

    Flesh out more Go language support.

--- src/.bashrc.d/golang.sh
+++ src/.bashrc.d/golang.sh
@@ -1,9 +1,40 @@
 # $HOME/.bashrc.d/golang.sh
 # Go language support
 
-export GOROOT=$HOME/opt/go/current
-export GOBIN=$GOROOT/bin
-export PATH=$GOBIN:$PATH
+case `uname -s` in
+	*Linux*)
+		export GOOS="linux"
+		;;
+	*FreeBSD*)
+		export GOOS="freebsd"
+		;;
+	*Darwin*)
+		export GOOS="darwin"
+		;;
+	*)
+		# Do nothing.
+		;;
+esac
+
+case `uname -m` in
+	*x86_64* | \
+	*amd64*)
+		export GOARCH="amd64"
+		;;
+	*i*86*)
+		export GOARCH="386"
+		;;
+	*)
+		# Do nothing.
+		;;
+esac
+
+if [ -n "$GOARCH" -a -n "$GOOS" ]
+then
+	export GOROOT=$HOME/opt/go/current
+	export GOBIN=$GOROOT/bin
+	export PATH=$GOBIN:$PATH
+fi
 
 # Local Variables:
 # mode:shell-script
