72215c5f3de7075d2b50e0e4723ba99deffcb7ff
Nathaniel R Reindl <nrr@corvidae.org>
2011-10-19 13:14:37 +0000

    Add support for Google Go.

---
+++ src/.bashrc.d/golang.sh
@@ -0,0 +1,10 @@
+# $HOME/.bashrc.d/golang.sh
+# Go language support
+
+export GOROOT=$HOME/opt/go/current
+export GOBIN=$GOROOT/bin
+export PATH=$GOBIN:$PATH
+
+# Local Variables:
+# mode:shell-script
+# End:
