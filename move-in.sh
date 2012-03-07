db1dc35bf46d17e435db3ed28e14076db151408f
Nathaniel R Reindl <nrr@corvidae.org>
2012-03-07 05:26:02 -0600

    Fix install_into_HOME()'s 'cannot stat' problem by correcting the file names.

--- move-in.sh
+++ move-in.sh
@@ -6,6 +6,7 @@
 #
 
 TD=`mktemp -d XXXXXXX`
+REPO_OWNER="nrr"
 REPO_NAME="dotfiles-bashrc"
 
 maybe_create_ssh_directory() {
@@ -28,9 +29,9 @@ pull_files_from_git() {
 	# This relies on TD above.
 
 	repo_host="github.com"
-	repo_name="nrr/${REPO_NAME}"
+	repo_path="${REPO_OWNER}/${REPO_NAME}"
 	archive_path="tarball/master"
-	url="https://${repo_host}/${repo_name}/${archive_path}"
+	url="https://${repo_host}/${repo_path}/${archive_path}"
 
 	curl ${CURL_OPTS-""} -LsSf $url | tar -C $TD -xvzf -
 }
@@ -38,7 +39,7 @@ pull_files_from_git() {
 install_into_HOME() {
 	# This relies on TD above.
 
-	cp -Rf $TD/$REPO_NAME-*/src/.???* $HOME/
+	cp -Rf $TD/$REPO_OWNER-$REPO_NAME-*/src/.???* $HOME/
 	rm -rf $TD
 }
 
