46fbaa5e150258a6f2b93f7ff19a38044c89dc8f
Nathaniel R Reindl <nrr@corvidae.org>
2012-03-07 07:21:30 -0600

    Add /usr/local/bin and friends to the $PATH.

---
+++ src/.bashrc.d/zy-usr-local-bin.sh
@@ -0,0 +1,10 @@
+# $HOME/.bashrc.d/zy-usr-local-bin.sh.sh
+# Add /usr/local/bin and friends to the $PATH.
+
+PATH="/usr/local/bin:${PATH}"
+
+export PATH
+
+# Local Variables:
+# mode:shell-script
+# End:
