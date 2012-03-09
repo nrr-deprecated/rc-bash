4cc07bb70373159998468a854d9e1efb16098576
Nathaniel R Reindl <nrr@corvidae.org>
2012-03-09 11:37:26 -0600

    Tighten up some loose bolts in my perlbrew/local::lib setup.

--- src/.bashrc.d/perlbrew.sh
+++ src/.bashrc.d/perlbrew.sh
@@ -3,7 +3,11 @@
 
 # env PERLBREW_ROOT=$HOME/.perlbrew perlbrew init
 
+PERLBREW_ROOT=$HOME/.perlbrew
+
 [[ -s "$HOME/.perlbrew/etc/bashrc" ]] && source "$HOME/.perlbrew/etc/bashrc"
+
+export PERLBREW_ROOT
 
 # Local Variables:
 # mode:shell-script
