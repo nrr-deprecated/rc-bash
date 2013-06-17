7eef7668757e81f09434cdf5b9b2527f0e518bef
Nathaniel Reindl <nrr@corvidae.org>
2013-06-17 12:21:33 -0400

    Stop using rvm.

--- src/.bashrc.d/ruby-version-manager.sh
+++
@@ -1,9 +0,0 @@
-# $HOME/.bashrc.d/ruby-version-manager.sh
-# Install the rvm environment.
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
-# Local Variables:
-# mode:shell-script
-# End:
-
