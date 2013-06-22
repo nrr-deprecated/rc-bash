b195fe0a5ebc17c3bb4832bb234b5cde310b53a5
Nathaniel Reindl <nrr@corvidae.org>
2013-06-22 11:07:05 -0400

    Remove this obsolete shit. Set EDITOR appropriately.

---
+++ src/.bashrc.d/editor.sh
@@ -0,0 +1,2 @@
+export EDITOR=E
+unset FCEDIT VISUAL
--- src/.bashrc.d/emacs.sh
+++
@@ -1,8 +0,0 @@
-# $HOME/.bashrc.d/emacs.sh
-# Emacs support
-
-export PATH=/opt/emacs/current/bin:$PATH
-
-# Local Variables:
-# mode:shell-script
-# End:
